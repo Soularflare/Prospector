70f31917bb09b38fe80fbb8937ced6b9499b8c67
R_baroni <R_baroni@bitbucket.org>
2020-01-14 23:41:07 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express(); 				// express server
+
+app.get('/', (req, res) => {		//create new handler for http requests
+	res.send({ hi: 'there'});
+});
+
+const PORT = process.env.PORT || 5000; 		//check environment for Heroku-specified port(Dynamic port binding)
+app.listen(PORT);					//server listens on port 5000
