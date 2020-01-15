04bd15c9432e1150ca0f0c5c84669394475bc8ad
R_baroni <R_baroni@bitbucket.org>
2020-01-15 00:06:20 -0800

    new change

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express(); 				// express server
 
 app.get('/', (req, res) => {		//create new handler for http requests
-	res.send({ hi: 'there'});
+	res.send({ bye: 'buddy'});
 });
 
 const PORT = process.env.PORT || 5000; 		//check environment for Heroku-specified port(Dynamic port binding)
