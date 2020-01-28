4c60e4f16ad894726c75ccb541c1f8dc2ed4c3d2
R_baroni <R_baroni@bitbucket.org>
2020-01-28 14:14:46 -0800

    fixing build

---
+++ client/build/precache-manifest.51726bf3c0192b91af1dc2b6302f57ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bf6564dfc0751b3592882c01f30da2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "199f7bba2865f7b262e9",
+    "url": "/static/css/2.2623327d.chunk.css"
+  },
+  {
+    "revision": "199f7bba2865f7b262e9",
+    "url": "/static/js/2.b1aa9974.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.b1aa9974.chunk.js.LICENSE"
+  },
+  {
+    "revision": "97ba50186d1d4f22f6d9",
+    "url": "/static/js/main.2e34908f.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
